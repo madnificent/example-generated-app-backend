9984920015746c9e9b8fed1f527ad3d5a027fe34
Aad Versteden <madnificent@gmail.com>
2018-12-27 07:49:21 +0100

    Add generated dispatcher rules
    These dispatcher rules are the default generated rules which forward
    content to mu-cl-resources.  This is sufficient for our little demo
    app.

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -22,6 +22,40 @@ defmodule Dispatcher do
   #   Proxy.forward conn, path, "http://resource/themes/"
   # end
 
+  ## START GENERATED PROXY RULES
+  match "/pipelines/*path" do
+    Proxy.forward conn, path, "http://resource/pipelines/"
+  end
+  match "/steps/*path" do
+    Proxy.forward conn, path, "http://resource/steps/"
+  end
+  match "/docker-composes/*path" do
+    Proxy.forward conn, path, "http://resource/docker-composes/"
+  end
+  match "/container-items/*path" do
+    Proxy.forward conn, path, "http://resource/container-items/"
+  end
+  match "/container-groups/*path" do
+    Proxy.forward conn, path, "http://resource/container-groups/"
+  end
+  match "/container-relations/*path" do
+    Proxy.forward conn, path, "http://resource/container-relations/"
+  end
+  match "/pipeline-instances/*path" do
+    Proxy.forward conn, path, "http://resource/pipeline-instances/"
+  end
+  match "/services/*path" do
+    Proxy.forward conn, path, "http://resource/services/"
+  end
+  match "/stacks/*path" do
+    Proxy.forward conn, path, "http://resource/stacks/"
+  end
+  match "/statuses/*path" do
+    Proxy.forward conn, path, "http://resource/statuses/"
+  end
+  ## END GENERATED PROXY RULES
+
+
   match _ do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
